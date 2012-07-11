2172fe3d278daa3a401e49c771492b92dd7582ae
cyper <uniquejava@foxmail.com>
2012-07-12 03:09:17 +0800

    fixed bug001,bug002

--- src/hello/example/swt_table/Main.java
+++ src/hello/example/swt_table/Main.java
@@ -29,6 +29,8 @@ import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -60,9 +62,11 @@ public class Main {
 	public final Shell shell = new Shell(display);
 	private ViewForm viewForm = null;
 	private List<Row> list;
-	private Text prevFocusedText;
 	private Text focusedText;
 	private Text[][] textArray = null;
+
+	// canCallFocusGained is for bug 001
+	private boolean canCallFocusGained = true;
 
 	public Main() throws Exception {
 		configureShell();
@@ -125,10 +129,12 @@ public class Main {
 		list = new TestDao().querySql("select * from ORG");
 		setInput(table, list);
 
-		
-		
+		// fix bug 002
+		fixTableLinesClickBug(table);
+
 		// 对toolbar的点击事件 集中进行处理
 		Listener listener = new Listener() {
+
 			@Override
 			public void handleEvent(Event event) {
 				if (event.widget == add) {
@@ -136,35 +142,39 @@ public class Main {
 					// selectionIndex);
 				} else if (event.widget == viewSingleRow) {
 					if (layout.topControl == table) {
-						setInput4ColumnMode(columnModeTable,getFocus().y);
+						setInput4ColumnMode(columnModeTable, getFocus().y);
 						layout.topControl = columnModeTable;
 					} else {
 						layout.topControl = table;
 					}
+
+					// fix bug 001
+					canCallFocusGained = false;
 					contentPanel.layout();
-					
-					//点一下toolbar，text的焦点就丢失了，所以
+
+					// 点一下toolbar，text的焦点就丢失了，所以
 					focusedText.setFocus();
-				} else if(event.widget == prev){
-					//查看上一行数据
-					//先将焦点上移一格
+					canCallFocusGained = true;
+
+				} else if (event.widget == prev) {
+					// 查看上一行数据
+					// 先将焦点上移一格
 					Point focus = getFocus();
-					//如果不是第一行才可以上移
-					if (focus.y>=2) {
-						focusedText = textArray[focus.y-1][focus.x];
+					// 如果不是第一行才可以上移
+					if (focus.y >= 2) {
+						focusedText = textArray[focus.y - 1][focus.x];
 						focusedText.setFocus();
-						setInput4ColumnMode(columnModeTable,getFocus().y);
+						setInput4ColumnMode(columnModeTable, getFocus().y);
 					}
-				} else if(event.widget == next){
-					//查看下一行数据
-					//先将焦点下移一格
+				} else if (event.widget == next) {
+					// 查看下一行数据
+					// 先将焦点下移一格
 					Point focus = getFocus();
-					//如果不是最后一行才可以下移，注意最后一行的y坐标是textArray.length-1
-					System.out.println(textArray.length);
-					if (focus.y < textArray.length-1) {
-						focusedText = textArray[focus.y+1][focus.x];
+					// 如果不是最后一行才可以下移，注意最后一行的y坐标是textArray.length-1
+					if (focus.y < textArray.length - 1) {
+						focusedText = textArray[focus.y + 1][focus.x];
 						focusedText.setFocus();
-						setInput4ColumnMode(columnModeTable,getFocus().y);
+						setInput4ColumnMode(columnModeTable, getFocus().y);
 					}
 				}
 
@@ -179,6 +189,40 @@ public class Main {
 
 	}
 
+	private void fixTableLinesClickBug(final Table table) {
+		final int headerHeight = table.getHeaderHeight();
+		final int itemHeight = table.getItemHeight();
+		table.addMouseListener(new MouseAdapter() {
+			public void mouseDown(MouseEvent e) {
+				// 当光标点在表格的线上时，文本框会失去焦点..no!不可以
+				// 33,47,61,75, 为了解决这个bug,首先观察每条线的y坐标，然后将 这个坐标偏上的单元格选中
+				// headerHeight=20, itemHeight=14
+				// 得出应选中单元格y坐标的通项式为(e.y-33)/14+1,注意必须为14.0而不是14
+				// 得出更一般的(e.y-headerHeight-itemHeight-1)/itemHeight+1
+
+				int n = (int) Math.ceil((e.y - headerHeight - itemHeight)
+						/ (itemHeight * 1.0)) + 1;
+
+				if (n >= 1 && n < textArray.length) {
+					textArray[n][getFocus().x].setFocus();
+					focusedText = textArray[n][getFocus().x];
+				} else if (n < 1) {
+					// PLSQL Developer不管鼠标点哪儿都至少有一行选中，所以这里我们也来实现.
+					textArray[1][getFocus().x].setFocus();
+					focusedText = textArray[1][getFocus().x];
+				} else if (n >= textArray.length) {
+					textArray[textArray.length - 1][getFocus().x].setFocus();
+					focusedText = textArray[textArray.length - 1][getFocus().x];
+				}
+			}
+		});
+	}
+
+	/**
+	 * 取得当前获得焦点的text的Point(其实是在数组中的位置)
+	 * 
+	 * @return
+	 */
 	private Point getFocus() {
 		Integer x = (Integer) focusedText.getData(COL_NUMBER);
 		Integer y = (Integer) focusedText.getData(ROW_NUMBER);
@@ -195,8 +239,8 @@ public class Main {
 		final HeaderRow tableHeader = (HeaderRow) list.get(0);
 		int rowCount = list.size();
 		int columnCount = tableHeader.keySet().size();
-		
-		//第一行留白算了。。以便text和整个表格完全保持一致.
+
+		// 第一行留白算了。。以便text和整个表格完全保持一致.
 		textArray = new Text[rowCount][columnCount];
 
 		// header
@@ -224,7 +268,7 @@ public class Main {
 			if (rowNumber % 2 == 1) {
 				tableItem.setBackground(getColor(COLOR_SQL_HIGHLIGHT));
 			}
-			
+
 			final int rrowNumber = rowNumber;
 
 			// bind text editor.
@@ -252,15 +296,19 @@ public class Main {
 				text.addFocusListener(new FocusListener() {
 					@Override
 					public void focusLost(FocusEvent e) {
-						tableItem.setText("");
-						textArray[rrowNumber][0].setText("");
+						if (canCallFocusGained) {
+							tableItem.setText("");
+							textArray[rrowNumber][0].setText("");
+						}
 					}
 
 					@Override
 					public void focusGained(FocusEvent e) {
-						tableItem.setText(">");
-						textArray[rrowNumber][0].setText(">");
-						focusedText = text;
+						if (canCallFocusGained) {
+							tableItem.setText(">");
+							textArray[rrowNumber][0].setText(">");
+							focusedText = text;
+						}
 					}
 				});
 
@@ -288,8 +336,7 @@ public class Main {
 
 		Row row = list.get(rowNumber);
 		// fix header is "","Rowx","Fields"
-		String[] tableHeader = new String[] { "", "Row" + (rowNumber),
-				"Fields" };
+		String[] tableHeader = new String[] { "", "Row" + (rowNumber), "Fields" };
 		int columnCount = 3;
 		{
 			TableColumn th = new TableColumn(columnModeTable, SWT.NONE);
